b16b9c242649b14e87e68e4e89f64438b5e91f21
aleetsaiya <aleetsaiya@gmail.com>
2022-01-19 18:36:37 +0800

    complete: create toast middle exercise

--- src/index.js
+++ src/index.js
@@ -10,19 +10,7 @@ import { projectAdded } from "./store/projects";
 import { userAdded } from "./store/users";
 
 const store = configureStore();
-
-store.subscribe(() => console.log("Store changed!", store.getState()));
-store.dispatch(userAdded({ name: "Alee" }));
-// store.dispatch(projectAdded({ name: "Project 1" }));
-// store.dispatch(bugAdded({ description: "Bug-1" }));
-// store.dispatch(bugAdded({ description: "Bug-2" }));
-// store.dispatch(bugAssignedToUser({ userId: 1, bugId: 2 }));
-// store.dispatch(bugResolved({ id: 1 }));
-
-// console.log("state:", store.getState());
-
-// const unResolved = getUnresolvedBugs(store.getState());
-// console.log("unResolved:", unResolved);
-
-// const bugs = getBugsByUser(2)(store.getState());
-// console.log("Bugs asign to user", bugs);
+store.dispatch({
+  type: "error",
+  payload: { message: "An error occured." },
+});
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 import logger from "./middleware/logger";
+import func from "./middleware/func";
+import toastify from "./middleware/toastify";
 
 export default function () {
   // when use configureStore, we don't need to check redux devTool extension
-  return configureStore({ reducer, middleware: [logger("my params")] });
+  return configureStore({ reducer, middleware: [toastify] });
 }
