d248f76fb4b210b6615e4ce20224fb382c022ee6
aleetsaiya <aleetsaiya@gmail.com>
2022-01-21 01:12:06 +0800

    chapter: finish solitary test and make a docs folder display

--- src/store/tests/bugs.spec.js
+++ src/store/tests/bugs.spec.js
@@ -1,10 +1,26 @@
-import { addBug } from "../bugs";
+// Solitary test DEMO
 
+import { addBug, bugAdded } from "../bugs";
+import { apiCallBegan } from "../api";
+
+// define a group of test "bugsSlice"
 describe("bugsSlice", () => {
+  // define a group of test "action creators"
   describe("action creators", () => {
+    // define a test "addBug"
     it("addBug", () => {
-      const result = addBug({ description: "a" });
-      const expected = {};
+      const bug = { description: "a" };
+      const result = addBug(bug);
+      const expected = {
+        type: apiCallBegan.type,
+        payload: {
+          url: "/bugs",
+          method: "post",
+          data: bug,
+          onSuccess: bugAdded.type,
+        },
+      };
+      expect(result).toEqual(expected);
     });
   });
 });
